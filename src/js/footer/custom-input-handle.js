b92bad7b4aaeed91aba8c665c22d3b3290de5284
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-08-18 11:23:29 +0200

    add data-original-attribut

--- src/js/footer/custom-input-handle.js
+++ src/js/footer/custom-input-handle.js
@@ -1,24 +1,49 @@
 
-  export function setupInputHandlers() {
-    const inputContainers = document.querySelectorAll('.js-custom-input-container');
+const maxLength = 30;
+
+export function setupInputHandlers() {
+  const customInputsRef = document.querySelectorAll('.js-custom-input');
+  customInputsRef.forEach(input => {
+    input.addEventListener('change', handleChange);
+    input.addEventListener('focus', handleFocus);
+    input.addEventListener('blur', handleBlur);
+  });
+}
+
+function truncateText(text, maxLength) {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + '...';
+  }
+  return text;
+}
+
+function handleChange(event) {
+    const input = event.target; 
+    const currentText = input.value;
+    const originalText = input.getAttribute('data-original-text');
+    if  (originalText) {
+        input.setAttribute('data-original-text', currentText);
+    }     
+    if (currentText.length > maxLength) {
+      input.value = truncateText(currentText, maxLength);
+    }
+  }
   
-    inputContainers.forEach(container => {
-      const input = container.querySelector('.js-custom-input');      
-      input.addEventListener('input', handleCustomInput);
-    });
+  function handleFocus(event) {
+    const input = event.target;
+    const originalText = input.getAttribute('data-original-text');
+    if (originalText) {
+      input.value = originalText;
+    }
   }
 
-  function handleCustomInput(event) {
-    const input = event.target;
-    const container = input.closest('.js-custom-input-container'); 
-    const textMeasurer = container.querySelector('.footer-text-measurer'); 
-
-    textMeasurer.textContent = input.value; 
-    const textWidth = textMeasurer.offsetWidth;  
-    const inputWidth = input.offsetWidth;  
-   
-    if (textWidth >= inputWidth) {
-      const maxLength = input.value.length; 
-      input.value = input.value.slice(0, maxLength) + '...';
+  function handleBlur(event) {
+    const input = event.target;   
+    const originalText = input.getAttribute('data-original-text');
+    if  (originalText) {
+        input.setAttribute('data-original-text', currentText);
+    }     
+    if (currentText.length > maxLength) {
+      input.value = truncateText(currentText, maxLength);
     }
   }
