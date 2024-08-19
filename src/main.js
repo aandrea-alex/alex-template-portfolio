1a7ee4a6a7cc559d948dab3192b567d64faeb412
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-08-19 17:12:10 +0200

    added projects

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import './js/header/mobile-menu-handler';
 import './js/about-me/accordion-handler';
 import './js/about-me/swiper-handler';
 
+import './js/projects/projects-handler';
+
 import './js/footer/footer-handler';
