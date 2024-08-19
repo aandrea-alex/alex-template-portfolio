c9227b81d12cec6e35daf971244d014345a0932b
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-08-19 17:22:45 +0200

    added faq section

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './js/header/header-handler';
 import './js/header/mobile-menu-handler';
 import './js/about-me/accordion-handler';
 import './js/about-me/swiper-handler';
-
+import './js/faq/faq-handler';
 import './js/projects/projects-handler';
 
 import './js/footer/footer-handler';
