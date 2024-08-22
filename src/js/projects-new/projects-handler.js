97763ee3bf09d75f15ad5eefe38e7dda3ea23f93
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-08-22 07:57:33 +0200

    Added keybord support in Swipers, deleted msge

--- src/js/projects-new/projects-handler.js
+++ src/js/projects-new/projects-handler.js
@@ -1,7 +1,7 @@
 import projectsListCreate from './projects-list-create';
 import { projectsUrl } from '../helpers/data-projects';
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Keyboard } from 'swiper/modules';
 
 let projectsSwiper;
 
@@ -13,13 +13,17 @@ async function initializeProjectsList() {
   try {
     await projectsListCreate(projectsUrl, projectsListRef);
     projectsSwiper = new Swiper('.projects-swiper-container', {
-      modules: [Navigation],
+      modules: [Navigation, Keyboard],
       slidesPerView: 1,
       spaceBetween: 0,
       loop: false,
       navigation: {
         nextEl: '.projects-btn-next',
         prevEl: '.projects-btn-prev',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
       },
       speed: 1000,
       effect: 'slide', // ('slide', 'fade', 'cube', 'coverflow', '')
